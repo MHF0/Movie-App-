c3aa28422dce34860fdbaa3c32422354d8d1615f
tetra-shayeb <mohammednm40@gmail.com>
2020-11-25 00:42:23 +0200

    Create React App

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
-function App() {
-  return (
-   <div>
-    </div>
-  );
-}
+
+import Header from './elements/Header';
+
+const App = () => <div> <Header /> Hello </div>
 
 export default App;
---
+++ my-app/src/config.js
@@ -0,0 +1,21 @@
+// The API_KEY should be in .env
+const API_URL = 'https://api.themoviedb.org/3/';
+const API_KEY = '1bfa430aada4409bfa6a3c5528128e8a';
+
+// Search
+const SEARCH_BASE_URL = `${API_URL}search/movie?api_key=${API_KEY}&query=`;
+const POPULAR_BASE_URL = `${API_URL}movie/popular?api_KEY=${API_KEY}`;
+
+const IMAGE_BASE_URL = 'http://image.tmdb.org/t/p/';
+//sizes
+const BACKDROP_SIZE = 'w1280';
+const POSTER_SIZE = 'w500';
+
+export {
+    SEARCH_BASE_URL,
+    POPULAR_BASE_URL,
+    API_KEY, API_KEY,
+    IMAGE_BASE_URL,
+    BACKDROP_SIZE,
+    POPULAR_BASE_URL
+}
