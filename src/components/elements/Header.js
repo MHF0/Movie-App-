7b94579c35522d5799c478bf333ebe835c0c1fa2
tetra-shayeb <mohammednm40@gmail.com>
2020-11-25 12:16:37 +0200

    Add styling for Grid & Hero image

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from '@reach/router';
 
-const Header = () => <div>Header</div>
+import RMDBLogo from '../image/reactMovie_logo.png';
+
+
+import { 
+    StyledHeader, 
+    StyledRMDBLogo,
+} from '../styels/StyledHeader';
+
+const Header = () => (
+<StyledHeader>
+  <div className="header-content">
+    <Link to="/">
+      <StyledRMDBLogo src={RMDBLogo} alt="rmdb-logo" />
+    </Link>
+    
+  </div>
+</StyledHeader>
+)
 
 export default Header;
